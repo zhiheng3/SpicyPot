b5602cc267a505ac0889b67477d2dc2e031f2e3a
Zhiheng <zhiheng3@live.cn>
2014-11-09 23:29:52 +0800

    structure

--- index.php
+++ index.php
@@ -59,34 +59,18 @@ class WechatCallbackAPI
 
       	//extract post data
 		if (!empty($postStr)){
-            $parseObj = new RequestParse();
-            $parseObj->parseXML($postStr);
-            
                 /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
                    the best way is to check the validity of xml by yourself */
                 libxml_disable_entity_loader(true);
               	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                	$contentStr = "Welcome to wechat world!";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
+                $test = new ResponseData();
+                $test->toUsername = $postObj->FromUserName;
+                $test->fromUsername = $postObj->ToUserName;
+                $test->content = trim($postObj->Content);
+                $test->createtime = time();
+                $test->msgType = "text";
+                $responseObj = new RequestResponse();
+                echo $response->response($test);
 
         }else {
         	echo "";
