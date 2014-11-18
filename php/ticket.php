2012f04bd7ad25a4124ebc3e69f2f3efca98f995
FCP <fcpatgoo@gmail.com>
2014-11-18 19:17:18 +0800

    test for no title

--- php/ticket.php
+++ php/ticket.php
@@ -46,9 +46,9 @@ class ticketHandler{
             $result->articleCount = 1;
 	        $result->articles = array();
 			$result->articles[0] = new Article();
-            $result->articles[0]->title = "抢票成功！";
-            $result->articles[0]->description = "抢票成功！";
-            $result->articles[0]->picUrl = "http://wx9.igeek.asia/img/31.png";
+            //$result->articles[0]->title = "抢票成功！";
+            //$result->articles[0]->description = "抢票成功！";
+            //$result->articles[0]->picUrl = "http://wx9.igeek.asia/img/31.png";
             $result->articles[0]->url = "http://wx9.igeek.asia/ActivityInfo.html";
         }
         else{
