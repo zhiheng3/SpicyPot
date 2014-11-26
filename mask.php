f96bd43c42bf07eb553c9418591b61748b33ffaa
FCP <fcpatgoo@gmail.com>
2014-11-26 13:35:40 +0800

    mask

--- mask.php
+++ mask.php
@@ -9,4 +9,20 @@ if ($method == 'unbind'){
     $unbindResult = $dataapi->unbind($openId, $studentId);
     echo json_encode($unbindResult);
 }
+
+else if($method == 'refundTicket'){
+    $openId = $_POST['openid'];
+    $ticketId = $_POST['ticketid'];
+    $dataapi = new dataAPI();
+    $refundTicketResult = $dataapi->refundTicket($openId, $ticketId);
+    echo json_encode($refundTicketResult);
+}
+
+else if($method == 'takeSeat'){
+    $ticketId = $_POST['ticketid'];
+    $seatId = $_POST['seatid'];
+    $dataapi = new dataAPI();
+    $takeSeatResult = $dataapi->takeSeat($ticketId, $seatId);
+    echo json_encode($takeSeatResult);
+}
 ?>
