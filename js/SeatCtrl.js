2b13936cb71320c150c3f951e6fbc960e492a3fa
zhiheng3 <zhiheng3@live.cn>
2014-12-23 22:46:41 +0800

    Seat JS

---
+++ js/SeatCtrl.js
@@ -0,0 +1,188 @@
+/*
+Set or Get ViewBox
+Param: DOM, int, int, int, int
+*/
+
+function ViewBox(Dom, x, y, w, h){
+    var viewbox = $(Dom).prop("viewBox");
+    var val = viewbox.baseVal;
+    if (typeof(x) == "undefined"){
+        return val;
+    }
+    else{
+        viewbox.baseVal.x = x;
+        viewbox.baseVal.y = y;
+        viewbox.baseVal.width = w;
+        viewbox.baseVal.height = h;
+        $(Dom).prop("viewBox", viewbox); 
+    }
+}
+
+/*
+Move SVG
+Param: DOM, int, int, Bool
+*/
+
+function Move(Dom, deltaX, deltaY, scale){
+    var viewbox = ViewBox(Dom);
+    
+    var X = viewbox.x + deltaX;
+    var Y = viewbox.y + deltaY;
+    var maxW = $(Dom).attr("maxw");
+    var maxH = $(Dom).attr("maxh");
+    
+    if (X < 0)
+        X = 0;
+    if (Y < 0)
+        Y = 0;
+    if (X + viewbox.width > maxW)
+        X = maxW - viewbox.width;
+    if (Y + viewbox.height > maxH)
+        Y = maxH - viewbox.height;
+    ViewBox(Dom, X, Y, viewbox.width, viewbox.height);
+}
+
+/*
+Zoom SVG
+Param: DOM, double, int, int
+*/
+function Zoom(Dom, rate, centerX, centerY){
+    var viewbox = ViewBox(Dom);
+    var width = viewbox.width * rate;
+    var height = viewbox.height * rate;
+    var maxW = $(Dom).attr("maxw");
+    var maxH = $(Dom).attr("maxh");
+    
+    if (width > maxW || height > maxH){
+        ViewBox(Dom, 0, 0, maxW, maxH);
+    }
+    else if (centerX == undefined || centerY == undefined){
+        ViewBox(Dom, viewbox.x, viewbox.y, width, height);
+    }
+    else{
+        ViewBox(Dom, centerX - width / 2, centerY - height / 2, width, height);
+    }
+}
+
+function AddMoveListener(Dom){
+    $(Dom).on("mousedown", MousedownHandler);
+    $(Dom).on("mousemove", MousemoveHandler);
+    $(Dom).on("mouseup", MouseupHandler);
+    $(Dom).on("mouseleave", MouseleaveHandler);
+    
+    $(Dom).on("touchstart", TouchstartHandler);
+    $(Dom).on("touchmove", TouchmoveHandler);
+    $(Dom).on("touchend", TouchendHandler);
+}
+
+/*
+callback: function, param: seatname
+*/
+function AddSeatClickListener(Dom, callback){
+    $(Dom).on("click", ".seat_able", function(e){
+        if (Args.move.moved)
+            return false;
+    });
+} 
+
+/*
+Mouse Handler
+*/
+function MousedownHandler(e){
+    StartMove(e.clientX, e.clientY);
+    e.preventDefault();
+}
+
+function MousemoveHandler(e){
+    ProcessMove(e.clientX, e.clientY, e.currentTarget);
+    e.preventDefault();
+}
+
+function MouseupHandler(e){
+    EndMove(e);
+    e.preventDefault();
+}
+
+function MouseleaveHandler(e){
+    EndMove();
+    e.preventDefault();
+}
+
+/*
+Touch Handler
+*/
+function TouchstartHandler(e){
+    var touches = e.originalEvent.targetTouches;
+    if (touches.length == 1){
+        StartMove(touches[0].clientX, touches[0].clientY);
+    }
+    e.preventDefault();
+}
+
+function TouchmoveHandler(e){
+    var touches = e.originalEvent.targetTouches;
+    if (touches.length == 1){
+        ProcessMove(touches[0].clientX, touches[0].clientY, e.currentTarget);
+    }
+    e.preventDefault();
+}
+
+function TouchendHandler(e){
+    EndMove(e);
+    e.preventDefault();
+}
+
+/*
+Move Event
+*/
+function StartMove(x, y){
+    Args.move.x = x;
+    Args.move.y = y;
+    Args.move.start = true;
+    Args.move.moved = false;
+}
+
+function ProcessMove(x, y, Dom){
+    if (!Args.move.start)
+        return false;
+    var deltaX = x - Args.move.x;
+    var deltaY = y - Args.move.y;
+    
+    if (Math.abs(deltaX) + Math.abs(deltaY) < 10)
+        return false;
+    Move(Dom, -deltaX, -deltaY, true);
+    Args.move.x = x;
+    Args.move.y = y;
+    Args.move.moved = true;
+}
+
+function EndMove(e){
+    Args.move.start = false;
+    if (typeof(e) == "undefined")
+        return ;
+    if (!Args.move.moved){
+        Args.clickHandler(e.target.id);
+    }
+    else
+        alert("moved");
+}
+
+function updateThumb(canvas, viewbox){
+    var tmp = $("#svg_seat").clone();
+    $("#test").empty();
+    $("#test").append(tmp);
+}
+
+function drawViewBox(canvas, viewbox){
+}
+
+$(document).ready(function(){
+    //Init
+    Args = Object();
+    Args.move = Object();
+    Args.move.start = false;
+    Args.move.moved = false;
+    Args.move.x = 0;
+    Args.move.y = 0;
+    Args.clickHandler = function (id) {alert(id);};
+});
---
+++ js/SeatEdit.js
@@ -0,0 +1,129 @@
+function SetSVGAttr(Dom, attr, value){
+    var svgns = "http://www.w3.org/2000/svg";
+    if (typeof(value) != "undefined"){
+        Dom.setAttributeNS(null, attr, value);
+    }
+    return Dom;
+}
+
+/*
+SVG Info
+width, height, maxW, maxH, id
+*/
+
+/*
+Create a SVG
+Param: DOM, Object(int, int, int, int, string)
+*/
+function CreateSVG(Dom, args){
+    var svgns = "http://www.w3.org/2000/svg";
+    var svg = document.createElementNS(svgns, "svg");
+    
+    SetSVGAttr(svg, "version", 1.1);
+    
+    SetSVGAttr(svg, "width", args.width);
+    SetSVGAttr(svg, "height", args.height);
+    SetSVGAttr(svg, "maxw", args.maxW);
+    SetSVGAttr(svg, "maxh", args.maxH);
+    SetSVGAttr(svg, "viewBox", "0 0 " + args.maxW.toString() + " " + args.maxH.toString());
+    SetSVGAttr(svg, "id", args.id);
+    
+    $(Dom).append(svg);
+    return svg;
+}
+
+/*
+Seat Info
+x, y, width, height: int
+fillcolor: HTML color
+seatname, seatclass: string
+
+Seat Class
+seat_able | seat_unable | seat_multi
+*/
+
+/*
+Create a seat
+Param: DOM, int, int, int, int, color, string, string
+*/
+function CreateSeat(Dom, args){
+    var svgns = "http://www.w3.org/2000/svg";
+    var rect = document.createElementNS(svgns, "rect");
+    
+    SetSVGAttr(rect, "x", args.x);
+    SetSVGAttr(rect, "y", args.y);
+    SetSVGAttr(rect, "width", args.width);
+    SetSVGAttr(rect, "height", args.height);
+    SetSVGAttr(rect, "fill", args.fill);
+    
+    var r = parseInt(Math.min(args.width, args.height) / 4);
+    SetSVGAttr(rect, "stroke", "#ffffff");
+    SetSVGAttr(rect, "rx", r);
+    SetSVGAttr(rect, "ry", r);
+    
+    SetSVGAttr(rect, "id", args.seatname);
+    SetSVGAttr(rect, "class", args.seatclass);
+    
+    $(Dom).append(rect);
+    return rect;
+}
+
+/*
+Save Seats SVG
+Param: DOM
+Return: string
+*/
+function SaveSVG(Dom){
+    return $(Dom).parent().html();
+}
+
+/*
+Load Seats SVG
+Param: DOM, string
+*/
+function LoadSVG(Dom, svgtext){
+    $(Dom).html(svgtext);
+    var svg = $(Dom).children();
+    AddMoveListener(svg);
+    return svg;
+}
+
+
+/*
+
+function loadSVG(Rpaper, json_data){
+    var data = JSON.parse(json_data);
+    Rpaper.clear();
+    Rpaper.add(data.SVG);
+    var i = 0;
+    Rpaper.forEach(function(e){
+        e.data("position", data.info[i].position);
+    });
+    Rpaper.maxW = data.maxW;
+    Rpaper.maxH = data.maxH;
+    Rpaper.setViewBox(0, 0, Rpaper.maxW, Rpaper.maxH);
+    $(Rpaper.canvas).prop("maxW", Rpaper.maxW);
+    $(Rpaper.canvas).prop("maxH", Rpaper.maxH);
+    
+    AddListener(Rpaper.canvas);
+}
+
+function saveSVG(Rpaper_data){
+    var i = 0;
+    var result = Object();
+    result.SVG = Array();
+    result.info = Array();
+    result.maxW = Rpaper_data.maxW;
+    result.maxH = Rpaper_data.maxH;
+    Rpaper_data.forEach(function(e){
+        if (e.data("position") != undefined){
+            result.SVG[i] = e.attrs;
+            result.SVG[i].type = e.type;
+            result.info[i] = Object();
+            result.info[i].position = e.data("position");
+            i++;
+        }
+    });
+    return JSON.stringify(result);
+}
+*/
