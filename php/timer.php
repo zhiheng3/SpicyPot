e791fa239cf4abc058dccfe8fbd546ecb7b007b5
FCP <fcpatgoo@gmail.com>
2014-12-24 23:05:27 +0800

    Time compare fixed

--- php/timer.php
+++ php/timer.php
@@ -17,23 +17,9 @@ class Timer{
                 $expireTime = $activity["message"]["end_time"];
                 $prematureTime = $activity["message"]["start_time"];
             }
-            $yearExpire = substr($expireTime, 0, 4);
-            $monthExpire = substr($expireTime, 5, 2);
-            $dayExpire = substr($expireTime, 8, 2);
-            $hourExpire = substr($expireTime, 11, 2);
-            $minExpire = substr($expireTime, 14, 2);
-            
-            $yearPremature = substr($prematureTime, 0, 4);
-            $monthPremature = substr($prematureTime, 5, 2);
-            $dayPremature = substr($prematureTime, 8, 2);
-            $hourPremature = substr($prematureTime, 11, 2);
-            $minPremature = substr($prematureTime, 14, 2);
-            if(date("Y") >= $yearExpire && date("m") >= $monthExpire && date("d") >= $dayExpire && date("H") >= $hourExpire && date("i") >= $minExpire){
-                return "Expired";
-            }
-            else if(date("Y") <= $yearPremature && date("m") <= $monthPremature && date("d") <= $dayPremature && date("H") <= $hourPremature && date("i") <= $minPremature){
-                return "Premature";
-            }
+
+            if(strtotime(date("Y-m-d H:i:s")) > strtotime($expireTime)) return "Expired";
+            else if(strtotime(date("Y-m-d H:i:s")) < strtotime($prematureTime)) return "Premature";
             else return "OK";
         }
     }
